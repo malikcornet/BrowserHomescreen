48b005df4726a6ddcefa9aad0c0314273af15f69
cornetmalik <malikcornet@outlook.com>
2026-03-08 17:38:55 +0100

    fix icons on gh pages

--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -1,11 +1,12 @@
 import styles from "./StartButton.module.css";
+import windowsIcon from "../assets/icons/windows.avif";
 
 function StartButton() {
   return (
     <button type="button" className={styles.startButton} aria-label="Open Start menu">
       <img
         className={styles.logo}
-        src="src/assets/icons/windows.avif"
+        src={windowsIcon}
         alt="Start menu icon"
       />
       <span className={styles.label}>Start</span>
--- src/models/directoryitem.tsx
+++ src/models/directoryitem.tsx
@@ -1,9 +1,10 @@
 import { FileSystemItemBase } from "./filesystemitem";
+import directoryClosedIcon from "../assets/icons/directory_closed.avif";
 
 export class DirectoryItem extends FileSystemItemBase {
   public Children: FileSystemItemBase[];
 
-  constructor(name: string, children: FileSystemItemBase[], icon = "src/assets/icons/directory_closed.avif") {
+  constructor(name: string, children: FileSystemItemBase[], icon = directoryClosedIcon) {
     super(icon, name);
     this.Children = children;
   }
