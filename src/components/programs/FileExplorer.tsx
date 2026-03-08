004c8f5a5fb83e2939dc29644d69fef2e6b71627
cornetmalik <malikcornet@outlook.com>
2026-03-08 16:21:40 +0100

    First version fileexplorer

--- src/components/programs/FileExplorer.tsx
+++ src/components/programs/FileExplorer.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { DirectoryItem } from "../../models/directoryitem";
 import { FileItem } from "../../models/fileitem";
 import DirectoryIcon from "../icons/DirectoryIcon";
@@ -6,12 +7,18 @@ import programStyles from "./Program.module.css";
 import styles from "./FileExplorer.module.css";
 
 function FileExplorer({directoryItem}: {directoryItem: DirectoryItem}) {
+  const [currentDirectory, setCurrentDirectory] = useState(directoryItem);
+
+  const handleDoubleClick = (clickedDirectory: DirectoryItem) => {
+    setCurrentDirectory(clickedDirectory);
+  };
+
   return (
     <div className={programStyles.programParent} >
       <div className={styles.childrenGrid}>
-        {directoryItem.Children.map((item, index) => {
+        {currentDirectory.Children.map((item, index) => {
           if (item instanceof DirectoryItem) {
-            return <DirectoryIcon key={`dir-${item.name}-${index}`} directoryItem={item} />;
+            return <DirectoryIcon key={`dir-${item.name}-${index}`} directoryItem={item} onDoubleClick={handleDoubleClick}/>;
           }
 
           if (item instanceof FileItem) {
